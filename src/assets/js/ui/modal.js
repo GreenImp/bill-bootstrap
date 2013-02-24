072a5f3960719a693d0b6464cf35f803b887b791
Green Imp <lee@greenimp.co.uk>
2013-02-24 02:30:03 +0000

    Adds base Modal dialogue
    Bill's modal dialogue is a simple wrapper for nyroModal: http://nyromodal.nyrodev.com/
    
    It needs to be fully tested and styled.
    
    Refs #31

---
+++ src/assets/js/ui/modal.js
@@ -0,0 +1,20 @@
+/**
+ * Copyright GreenImp Web - greenimp.co.uk
+ *
+ * Author: GreenImp Web
+ * Date Created: 24/02/13 01:24
+ */
+
+
+/*> jquery.nyroModal.custom.min.js */
+
+//methods.modal = $.fn.nyroModal;
+//methods.modal = 'hi';
+methods.modal = $.fn.nyroModal;
+
+$('.modal').nyroModal();
+
+// call the accordion functionality
+//console.log(methods);
+//console.log(methods.modal);
+//$('.modal').bill('modal');
