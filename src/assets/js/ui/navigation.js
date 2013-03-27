902598a139b696930814ee29b233ad0af6e23f73
Green Imp <lee@greenimp.co.uk>
2013-03-27 23:59:15 +0000

    Adds styling and JS funcionality to navigation
    The sliding. on small screens, doesn't work correctly as sub-nav overlays each other.
    
    Refs #27

--- src/assets/js/ui/navigation.js
+++ src/assets/js/ui/navigation.js
@@ -14,15 +14,21 @@ methods.navigation = function(){
 		// loop through all of the li elements and check if they have children
 		$nav.addClass('dynamic').wrapInner('<div class="inner"></div>').find('.inner > ul').find('li').each(function(){
 					var $item = $(this),					// the li element
-							$children = $item.children('ul');	// any child ul elements
+						$children = $item.children('ul');	// any child ul elements
 					if($children.length){
 						// the item has sub-navigation
 						$item.addClass('hasSub');	// apply hasSub class
 
 						// add a back button to the sub-nav (used for small screens)
-						$children.prepend('<li class="backBtn">Back</li>');
+						$children.each(function(){
+							var $child = $(this);
+							if(!$child.children('.backBtn:first').length){
+								$child.prepend('<li class="backBtn">' + ($nav.attr('data-text-back') || 'Back') + '</li>');
+							}
+						});
 					}
 				});
+		var $inner = $nav.children('.inner:first');
 
 		/**
 		 * Add click handler for showing sub navigation on smaller screens
@@ -33,10 +39,16 @@ methods.navigation = function(){
 
 			if(viewport && (viewport.width <= 767)){
 				// we're on a small screen
-				$nav.children('.inner').stop(true, true).animate({left: '-100%'}, animSpeed);
 
-				e.preventDefault();
+				// check the clicked element - ensure that it's not a child of hasSub
+				var target = $(e.target);
+				if(target.hasClass('hasSub') || ((target.prop('tagName').toUpperCase() != 'UL') && target.parent().hasClass('hasSub'))){
+					// we're on an actual hasSub element
+					$inner.stop(true, true).animate({left:'-=' + parseFloat($nav.innerWidth())}, animSpeed);
+					e.preventDefault();
+				}
 			}
+			e.stopPropagation();
 		});
 
 		$nav.find('.backBtn').on('click', function(e){
@@ -45,9 +57,22 @@ methods.navigation = function(){
 
 			if(viewport && (viewport.width <= 767)){
 				// we're on a small screen
-				$nav.children('.inner').stop(true, true).animate({left: 0}, animSpeed);
+				$inner.stop(true, true).animate({left:'+=' + parseFloat($nav.innerWidth())}, animSpeed);
 
 				e.preventDefault();
+			}
+			e.stopPropagation();
+		});
+
+
+		$(window).on('resize', function(){
+			var viewport = $.viewport() || null;
+
+			if(viewport && (viewport.width <= 767)){
+				$nav.height($inner.outerHeight());
+			}else{
+				$nav.removeAttr('style');
+				$inner.removeAttr('style');
 			}
 		});
 	});
