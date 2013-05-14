38671ba86573d694304431e4ffcd75885d601e1b
GreenImp <lee@greenimp.co.uk>
2013-05-14 23:58:51 +0100

    Updates Slider JS for core JS
    Refs #59

--- src/assets/js/ui/slider.js
+++ src/assets/js/ui/slider.js
@@ -1,24 +1,27 @@
 /**
- * Copyright GreenImp Web - greenimp.co.uk
+ * Bill.slider
  *
- * Author: GreenImp Web
- * Date Created: 16/03/13 18:46:38
+ * Image slider
  */
-
-
 /*> jquery.flexslider-min.js */
-;methods.slider = $.fn.flexslider;
 
-// add the slider initializer to the onLoad call
-// it needs to be onLoad, otherwise it doesn't detect image heights correctly.
-onLoad[onLoad.length] = function(){
-	// call the slider functionality
-	$('.slider').each(function(){
-		var slider = $(this);
-		slider.addClass('flexslider').bill('slider', {
-			animation:slider.hasClass('fade') ? 'fade' : 'slide',
+;(function($, window, document, undefined){
+	"use strict";
+
+	Bill.libs.slider = {
+		name:'Notice',
+		version:'0.1.0',
+		nameSpace:Bill.eventNameSpace + '.notice',
+		options:{
+			animation:'slide',
 			slideshowSpeed:4000,
 			smoothHeight:true
-		});
-	});
-};
+		},
+		init:function(scope, method, options){
+			this.scope = scope || this.scope;
+
+			var $elm = $(this.scope);
+			$elm.addClass('flexslider').flexslider($.extend(this.options, (typeof method === 'object') ? method : options));
+		}
+	};
+})(jQuery, window, document);
