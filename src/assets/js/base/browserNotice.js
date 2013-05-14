f94e6a00bb55b6894796d9eeae4ec347e9d4873f
GreenImp <info@greenimp.co.uk>
2013-05-14 14:00:45 +0100

    Updates browserNotice to use new Bill.js
    Refs #59

--- src/assets/js/base/browserNotice.js
+++ src/assets/js/base/browserNotice.js
@@ -1,91 +1,117 @@
 /**
+ * Bill.browserNotice
+ *
  * Checks if the current browser is supported.
  * If not, a message is displayed to the user,
  * to alert them that they are using an out-dated
  * browser.
  *
+ * Yes, browser sniffing is BAD, but sometimes you
+ * seriously, just don't want to provide support
+ * for a certain browser (IE 8<, for example).
+ *
  * 'options' contains a list of browsers and the
  * earliest supported version number.
  * By default, ie support is set to 9.
  * All others have full support.
- *
- * @param options
- * @return {*}
  */
-;methods.browserNotice = function(options){
-	'use strict';
+;(function($, window, document, undefined){
+	"use strict";
 
-	// list of browsers to check for
-	options = $.extend({
-		ie:9,
-		ff:null,
-		chrome:null,
-		safari:null,
-		opera:null
-	}, options);
+	Bill.libs.browserNotice = {
+		name:'Browser Notice',
+		version:'0.1.0',
+		options:{
+			browsers:{
+				ie:9,
+				ff:null,
+				chrome:null,
+				safari:null,
+				opera:null
+			},
+			animSpeed:600,
+			infoURL:'http://browsehappy.com/',
+			closable:false
+		},
+		init:function(scope, method, options){
+			return this.each(function(i){
+				this.scope = scope || this.scope;
 
-	// check if the browser is supported or not
-	var supported = true,
-		userAgent = navigator.userAgent;
-	if(options.ie && !isNaN(options.ie) && (/MSIE (\d+\.\d+);/.test(userAgent))){
-		// browser is IE
-		if(parseFloat(RegExp.$1) < parseFloat(options.ie)){
-			// version not supported
-			supported = false;
-		}
-	}else if(options.ff && !isNaN(options.ff) && (/Firefox[\/\s](\d+\.\d+)/.test(userAgent))){
-		// browser is Firefox
-		if(parseFloat(RegExp.$1) < parseFloat(options.ff)){
-			// version not supported
-			supported = false;
-		}
-	}else if(options.chrome && !isNaN(options.chrome) && (/Chrome[\/\s](\d+\.\d+)/.test(userAgent))){
-		// browser is Chrome
-		if(parseFloat(RegExp.$1) < parseFloat(options.chrome)){
-			// version not supported
-			supported = false;
-		}
-	}else if(options.safari && !isNaN(options.safari) && (/Version[\/\s](\d+\.\d+)(.\d+)*\s+Safari/.test(userAgent))){
-		// browser is Safari
-		if(parseFloat(RegExp.$1) < parseFloat(options.safari)){
-			// version not supported
-			supported = false;
-		}
-	}else if(options.opera && !isNaN(options.opera) && (/Opera.*?Version[\/\s](\d+\.\d+)/.test(userAgent))){
-		// browser is Opera
-		if(parseFloat(RegExp.$1) < parseFloat(options.opera)){
-			// version not supported
-			supported = false;
-		}
-	}
+				// only continue if the browser notice doesn't exist
+				if((i == 0) && !$('#browserNotice').length){
+					// list of browsers to check for
+					if(typeof method === 'object'){
+						// method is actually options
+						$.extend(true, this.options, method);
+					}else if(typeof method === 'object'){
+						$.extend(true, this.options, options);
+					}
 
-	if(!supported){
-		// browser not supported
-		var animSpeed = 600,
-			noticeBox = $('<div id="browserNotice" class="notice info fixed">' +
-					'<div class="container constrain">' +
-						'<div class="row">' +
-							'<div class="column eight">' +
-								'<p>It looks like you\'re using an outdated web browser. We don\'t support old browsers, as they have limited features and security issues.</p>' +
-								'<p>You should consider upgrading.</p>' +
+					// check if the browser is supported or not
+					var supported = true,
+						userAgent = navigator.userAgent,
+						browsers = this.options.browsers;
+					if(browsers.ie && !isNaN(browsers.ie) && (/MSIE (\d+\.\d+);/.test(userAgent))){
+						// browser is IE
+						if(parseFloat(RegExp.$1) < parseFloat(browsers.ie)){
+							// version not supported
+							supported = false;
+						}
+					}else if(browsers.ff && !isNaN(browsers.ff) && (/Firefox[\/\s](\d+\.\d+)/.test(userAgent))){
+						// browser is Firefox
+						if(parseFloat(RegExp.$1) < parseFloat(browsers.ff)){
+							// version not supported
+							supported = false;
+						}
+					}else if(browsers.chrome && !isNaN(browsers.chrome) && (/Chrome[\/\s](\d+\.\d+)/.test(userAgent))){
+						// browser is Chrome
+						if(parseFloat(RegExp.$1) < parseFloat(browsers.chrome)){
+							// version not supported
+							supported = false;
+						}
+					}else if(browsers.safari && !isNaN(browsers.safari) && (/Version[\/\s](\d+\.\d+)(.\d+)*\s+Safari/.test(userAgent))){
+						// browser is Safari
+						if(parseFloat(RegExp.$1) < parseFloat(browsers.safari)){
+							// version not supported
+							supported = false;
+						}
+					}else if(browsers.opera && !isNaN(browsers.opera) && (/Opera.*?Version[\/\s](\d+\.\d+)/.test(userAgent))){
+						// browser is Opera
+						if(parseFloat(RegExp.$1) < parseFloat(browsers.opera)){
+							// version not supported
+							supported = false;
+						}
+					}
+
+					if(!supported){
+						// browser not supported
+
+						// build the browser notice
+						$('<div id="browserNotice" class="notice info fixed"' + (this.options.closable ? ' data-close="1"' : '') + '>' +
+							'<div class="container constrain">' +
+								'<div class="row">' +
+									'<div class="column eight">' +
+										'<p>It looks like you\'re using an outdated web browser. We don\'t support old browsers, as they have limited features and security issues.</p>' +
+										'<p>You should consider upgrading.</p>' +
+									'</div>' +
+									(
+									this.options.infoURL ?
+										'<div class="column four">' +
+											'<a href="' + this.options.infoURL + '" title="Upgrade your browser" target="_blank" class="button">Find out more</a>' +
+										'</div>'
+										:
+										''
+									) +
+								'</div>' +
 							'</div>' +
-							'<div class="column four">' +
-								'<a href="http://browsehappy.com/" title="Upgrade your browser at Browse Happy" target="_blank" class="button">Find out more</a>' +
-							'</div>' +
-						'</div>' +
-					'</div>' +
-				'</div>');
+						'</div>')
+								// add the browser notice and slide it into view
+								.prependTo('body').hide().slideDown(this.options.animSpeed);
+					}
 
-		$('body')
-				// add the browser notice
-				.prepend(noticeBox)
-				// slide the whole page down, by the height of the notice (so page content doesn't get cut off)
-				.animate({'padding-top':noticeBox.outerHeight()}, animSpeed);
-
-		// slide the browser notice into view
-		noticeBox.hide().slideDown(animSpeed);
-	}
-
-	// return 'this' to allow chain-ability
-	return this;
-};
+					this.options.init = true;
+				}
+			});
+		}
+	};
+})(jQuery, window, document);
