f0c3205e2f3f0062f6bc671fcab219d3cc46062a
GreenImp <info@greenimp.co.uk>
2013-05-17 16:45:11 +0100

    Update modal JS
    Refs #66

--- src/assets/js/ui/modal.js
+++ src/assets/js/ui/modal.js
@@ -19,7 +19,31 @@
 		init:function(scope, method, options){
 			this.scope = scope || this.scope;
 
-			$(this.scope).fancybox($.extend(this.options, (typeof method === 'object') ? method : options));
-		}
+			if(typeof method === 'object'){
+				// method is actually options
+				$.extend(true, this.options, method);
+			}else if(typeof method === 'string'){
+				// call the method and return
+				return this[method].call(this, options);
+			}
+
+			if(!this.options.init){
+				this.on();
+			}
+
+			return this.options.init;
+		},
+		/**
+		 * Activates the library
+		 */
+		on:function(){
+			$(this.scope).find('[data-modal]').fancybox(this.options);
+
+			this.options.init = true;
+		},
+		/**
+		 * De-activates the library
+		 */
+		off:function(){}
 	};
 })(jQuery, window, document);
